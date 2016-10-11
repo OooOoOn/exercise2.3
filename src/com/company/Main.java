7d2c0a7c17d65704ade37f798ac15c6b40b4925e
Jon Olsson <aadrianswe@gmail.com>
2016-10-11 10:05:58 +0200

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import javax.swing.JOptionPane;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Skriv en mening med minst 2 ord och lämna ett mellanslag efter sista ordet:");
+        Scanner in = new Scanner(System.in);
+
+        String mening = JOptionPane.showInputDialog("");
+        mening.trim();
+        int sistaMellanslag = mening.lastIndexOf(" ");
+        int förstaMellanslag = mening.indexOf(" ");
+        String förstaOrd = mening.substring(0, förstaMellanslag);
+        String sistaOrd = mening.substring(sistaMellanslag);
+        int antaltecken = mening.length();
+
+        JOptionPane.showMessageDialog(null, "Du skrev en mening med " + antaltecken + " tecken.\nDet försts ordet var: " + förstaOrd + ".\nDet sista ordet var: " + sistaOrd);
+
+    }
+}
